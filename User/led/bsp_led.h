7f41d7c1a5046e311826b313ff3f1bf3fc925d57
alsmith <alsmith@163.com>
2021-07-15 20:01:18 +0800

    first commit

---
+++ User/led/bsp_led.h
@@ -0,0 +1,44 @@
+#ifndef __LED_H
+#define	__LED_H
+
+#include "stm32h7xx.h"
+
+//引脚定义
+/*******************************************************/
+//R 红色灯
+#define LED1_PIN                  GPIO_PIN_7                 
+#define LED1_GPIO_PORT            GPIOB                      
+#define LED1_GPIO_CLK_ENABLE()    __GPIOB_CLK_ENABLE()
+
+/************************************************************/
+
+
+/** 控制LED灯亮灭的宏，
+	* LED低电平亮，设置ON=0，OFF=1
+	* 若LED高电平亮，把宏设置成ON=1 ，OFF=0 即可
+	*/
+#define ON  GPIO_PIN_RESET
+#define OFF GPIO_PIN_SET
+
+/* 带参宏，可以像内联函数一样使用 */
+#define LED1(a)	HAL_GPIO_WritePin(LED1_GPIO_PORT,LED1_PIN,a)
+
+
+
+
+
+/* 直接操作寄存器的方法控制IO */
+#define	digitalHi(p,i)				{p->BSRRL=i;}			  //设置为高电平		
+#define digitalLo(p,i)				{p->BSRRH=i;}				//输出低电平
+#define digitalToggle(p,i)		{p->ODR ^=i;}			//输出反转状态
+
+
+/* 定义控制IO的宏 */
+#define LED1_TOGGLE		digitalToggle(LED1_GPIO_PORT,LED1_PIN)
+#define LED1_OFF			digitalHi(LED1_GPIO_PORT,LED1_PIN)
+#define LED1_ON				digitalLo(LED1_GPIO_PORT,LED1_PIN)
+
+
+void LED_GPIO_Config(void);
+
+#endif /* __LED_H */
